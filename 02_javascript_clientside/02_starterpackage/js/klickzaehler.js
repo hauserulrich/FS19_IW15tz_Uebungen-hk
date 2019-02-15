e8837a4040155661e0fee0d3d4a1a229d60a1f46
Ulrich Hauser-Ehninger <ulrich.hauser@htwchur.ch>
2019-02-15 09:18:47 +0100

    working card with counter

--- 02_javascript_clientside/02_starterpackage/js/klickzaehler.js
+++ 02_javascript_clientside/02_starterpackage/js/klickzaehler.js
@@ -1,3 +1,12 @@
 function klickZaehler() {
-  console.log("Hallo Velo");
+    if (typeof(Storage) !== "undefined") {
+        if (window.localStorage.counterClick) {
+            window.localStorage.counterClick = Number(window.localStorage.counterClick) + 1;
+        } else {
+            window.localStorage.counterClick = 1;
+        }
+        document.getElementById("howmany").innerHTML = window.localStorage.counterClick;
+    } else {
+        alert("no local storage");
+    }
 }
